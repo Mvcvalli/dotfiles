a2e526df8d4118eabbf3383e467406b27ae52066
Mason <102649685+Mvcvalli@users.noreply.github.com>
2022-12-10 06:14:35 +0000

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -25,40 +25,13 @@ function fish_prompt
 	set_color normal
 end
 
-# Colors
-set -U fish_color_normal normal
-set -U fish_color_command
-set -U fish_color_quote
-set -U fish_color_redirection
-set -U fish_color_end
-set -U fish_color_error
-set -U fish_color_param
-set -U fish_color_comment
-set -U fish_color_match
-set -U fish_color_selection --reverse
-set -U fish_color_search_match --reverse
-set -U fish_color_history_current
-set -U fish_color_operator
-set -U fish_color_escape
-set -U fish_color_cwd normal
-set -U fish_color_cwd_root normal
-set -U fish_color_valid_path
-set -U fish_color_autosuggestion brblack
-set -U fish_color_user normal
-set -U fish_color_host normal
-set -U fish_color_cancel
-set -U fish_pager_color_prefix --underline
-set -U fish_pager_color_progress brblack
-set -U fish_pager_color_completion normal
-set -U fish_pager_color_description brblack
-set -U fish_pager_color_selected_background --reverse
-
 # Automatically install fundle (package manager)
 if not functions -q fundle; eval (curl -sfL https://git.io/fundle-install); end
 
 # Plugins
 fundle plugin "edc/bass"
 fundle plugin "jorgebucaran/autopair.fish"
+fundle plugin "oh-my-fish/plugin-wifi-password"
 fundle init
 
 # Abbreviations
